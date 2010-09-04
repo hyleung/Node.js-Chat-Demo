a2afea3ad05897907f4e8e27f9cf3b8cd21b7478
H.Y. Leung <hy.leung@gmail.com>
2010-09-04 15:08:34 -0700

    Added client-side js code for Faye client

--- public/js/jquery.livestats.js
+++ public/js/jquery.livestats.js
@@ -1,1 +1,29 @@
 //jquery.livestats.js
+function LiveStatsClient () {
+	if(!(this instanceof arguments.callee)){
+		return new arguments.callee(arguments);
+	}
+	var self=this;
+	this.init = function(){
+		//call a function to setup a new Faye client
+		self.setupBayeuxHandlers();
+	};
+	
+	this.setupBayeuxHandlers = function(){
+		//hard-coding the url, but will use config.json later
+		self.client=new Faye.Client('http://localhost:8000/faye',{
+			timeout:120
+		});
+		//setup a path to subscribe to events at
+		self.client.subscribe('/stat',function(message) {
+			console.log('MESSAGE',message);
+		});
+	};
+	
+	this.init();
+};
+
+var liveStatsClient;
+jQuery(function(){
+	liveStatsClient = new LiveStatsClient();
+});
