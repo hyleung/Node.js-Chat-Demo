d23a9774fef76db0220ebf54086af7ced97301cb
H.Y. Leung <hy.leung@gmail.com>
2010-09-04 13:41:54 -0700

    Added error handling to server.js

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
 require.paths.unshift(__dirname + "/vendor");
 
+//add listener for uncaught exceptions
+process.addListener('uncaughtException',function(err,stack) {
+	console.log('----------------------------');
+  console.log('ExceptionL ' + err);
+	console.log(err.stack);
+	console.log('----------------------------');
+
+});
+
 var LiveStats = require('./lib/livestats');
 
 new LiveStats({
